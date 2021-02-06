2177d32bf8022c0d84114b13c1c28c42153a7cad
betocastillo86 <castillopradagabriel@gmail.com>
2021-02-05 20:03:48 -0500

    Intial commit - Solution created

---
+++ S4N.LuchToHome.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace S4N.LuchToHome.ConsoleApplication
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
