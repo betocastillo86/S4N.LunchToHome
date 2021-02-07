6498bcf9f04ecf059b2102e2b7d8d0803b1e474e
betocastillo86 <castillopradagabriel@gmail.com>
2021-02-07 11:53:19 -0500

    Add file helper implementation

---
+++ S4N.LunchToHome.Infrastructure/Helpers/FileHelper.cs
@@ -0,0 +1,22 @@
+﻿using System.IO;
+
+namespace S4N.LunchToHome.Infrastructure.Helpers
+{
+    public class FileHelper : IFileHelper
+    {
+        public void WriteContentOnFile(string path, string content, string header = null)
+        {
+            bool appendHeader = !File.Exists(path);
+
+            using (var file = new System.IO.StreamWriter(path, true))
+            {
+                if (appendHeader)
+                {
+                    file.WriteLine(header);
+                }
+
+                file.WriteLine(content);
+            }
+        }
+    }
+}
