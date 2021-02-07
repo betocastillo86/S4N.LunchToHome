a7848ca19d719a44975f75d642bca2bcdd5af0b1
betocastillo86 <castillopradagabriel@gmail.com>
2021-02-07 12:15:37 -0500

    Added IRepositoryImplementation

---
+++ S4N.LunchToHome.Infrastructure/Persistence/MemoryRepository.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using S4N.LunchToHome.Application.Common;
+using S4N.LunchToHome.Domain.Entities;
+
+namespace S4N.LunchToHome.Infrastructure.Persistence
+{
+    public class MemoryRepository<T> : IRepository<T> where T : BaseEntity
+    {
+        private readonly string tableName;
+
+        public MemoryRepository()
+        {
+            tableName = typeof(T).Name;
+
+            if (!Tables.ContainsKey(tableName))
+            {
+                Tables.Add(tableName, new List<T>());
+            }
+        }
+
+        internal static IDictionary<string, IList<T>> Tables = new Dictionary<string, IList<T>>();
+
+        public IQueryable<T> Items
+        {
+            get
+            {
+                return Tables[this.tableName].AsQueryable();
+            }
+        }
+
+        private IList<T> Data
+        {
+            get
+            {
+                return Tables[this.tableName];
+            }
+        }
+
+        public Task DeleteAsync(T entity)
+        {
+            this.Data.Remove(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task InsertAsync(T entity)
+        {
+            this.Data.Add(entity);
+            return Task.CompletedTask;
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
